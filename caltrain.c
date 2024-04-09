c35978c4cd9e51d6a8597568a1f02e2e67bd3b8b
Amen Mohamed <145727573+WorldisAmen@users.noreply.github.com>
2024-04-09 14:19:36 +0200

    Add files via upload

---
+++ caltrain.c
@@ -0,0 +1,52 @@
+#include <pthread.h>
+#include "caltrain.h"
+
+void station_init(struct station *station) {
+
+    station->available_seats = 0;
+    station->passengers_waiting = 0;
+    station->passengers_boarded = 0;
+
+    pthread_mutex_init(&(station->mutex), NULL);
+    pthread_cond_init(&(station->train_arrival), NULL);
+    pthread_cond_init(&(station->train_departure), NULL);
+}
+
+void station_load_train(struct station *station, int count) {   
+    pthread_mutex_lock(&(station->mutex)); 
+    
+    if (!count || !station->passengers_waiting) { // no free seats available or no passengers waiting
+        pthread_mutex_unlock(&(station->mutex)); // release the lock and leave critical section
+        return; // train must leave the station promptly
+    }
+    station->available_seats = count;
+    pthread_cond_broadcast(&(station->train_arrival)); // wake up all passengers waiting for a free seat
+    pthread_cond_wait(&(station->train_departure), &(station->mutex)); // waiting for all passengers to get on board
+    station->available_seats = 0;
+    
+    pthread_mutex_unlock(&(station->mutex));
+}
+
+void station_wait_for_train(struct station *station) {
+    pthread_mutex_lock(&(station->mutex));
+
+    station->passengers_waiting++;
+    while (!station->available_seats)
+        pthread_cond_wait(&(station->train_arrival), &(station->mutex)); // waiting for a train with free seats
+
+    station->passengers_waiting--;
+    station->passengers_boarded++;
+    station->available_seats--;
+    
+    pthread_mutex_unlock(&(station->mutex));
+}
+
+void station_on_board(struct station *station) {
+    pthread_mutex_lock(&(station->mutex));
+
+    if (station->passengers_boarded == 0 || station->available_seats == 0) {
+        pthread_cond_signal(&(station->train_arrival)); // Signal train departure if no passengers to board or no available seats
+    }
+
+    pthread_mutex_unlock(&(station->mutex));
+}
---
+++ caltrain.h
@@ -0,0 +1,20 @@
+#include <pthread.h>
+
+
+struct station {
+	int available_seats;      // Number of available seats in the train
+    int passengers_waiting;   // Number of passengers waiting at the station
+    int passengers_boarded;   // Number of passengers who have boarded the train
+    
+    pthread_mutex_t mutex;    // Mutex for thread synchronization
+    pthread_cond_t train_arrival;    // Condition variable indicating the arrival of the train
+    pthread_cond_t train_departure;  // Condition variable indicating the departure of the train
+};
+
+void station_init(struct station *station);
+
+void station_load_train(struct station *station, int count);
+
+void station_wait_for_train(struct station *station);
+
+void station_on_board(struct station *station);
