0452620c6c01b280374240e7ebe084d9bfbcdbfc
Amen Mohamed <145727573+WorldisAmen@users.noreply.github.com>
2024-04-15 18:57:25 +0200

    Update caltrain.c

--- caltrain.c
+++ caltrain.c
@@ -18,12 +18,14 @@ void station_load_train(struct station *station, int count) {
     // no available seats available or no passengers waiting
     if (!count || !station->passengers_waiting) {
         pthread_mutex_unlock(&(station->mutex));
-        return; // train must leave the station promptly
+        return;
     }
 
     station->available_seats = count;
-    pthread_cond_broadcast(&(station->train_arrival)); // wake up all passengers waiting for a available seat
-    pthread_cond_wait(&(station->train_departure), &(station->mutex)); // waiting for all passengers to get on board
+    // broadcast all passengers waiting for train
+    pthread_cond_broadcast(&(station->train_arrival));
+    // waiting for all passengers to get on board
+    pthread_cond_wait(&(station->train_departure), &(station->mutex)); 
     station->available_seats = 0;
 
     pthread_mutex_unlock(&(station->mutex));
@@ -49,7 +51,8 @@ void station_on_board(struct station *station) {
     pthread_mutex_lock(&(station->mutex));
     
     station->passengers_boarded--;
-    if (!station->passengers_boarded && !(station->passengers_waiting && station->available_seats)) // all leaving passengers are on board, and no other passengers can get on the train
+     // if all leaving passengers are on board, and no other passengers can get on the train (no free seats or no waiting)
+    if (!station->passengers_boarded && !(station->passengers_waiting && station->available_seats))
         pthread_cond_signal(&(station->train_departure));
     
     pthread_mutex_unlock(&(station->mutex));
